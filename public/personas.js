fcc581dc74975c2d9d8c6dbcdd5b7aafb0e9211c
Emily Wilbur <emilywilbur@MacBookPro.attlocal.net>
2025-04-28 13:36:14 -0400

    personas.js added

---
+++ public/personas.js
@@ -0,0 +1,28 @@
+// personas.js
+export const personas = [
+  {
+    name: "Goth Mode",
+    styles: {
+      backgroundColor: "black",
+      color: "red",
+      fontFamily: "Creepster, sans-serif",
+    },
+    microcopy: {
+      button: "Fine, Submit it.",
+      tooltip: "This will probably break.",
+    },
+  },
+  {
+    name: "Cheerleader Mode",
+    styles: {
+      backgroundColor: "yellow",
+      color: "pink",
+      fontFamily: "Comic Sans, sans-serif",
+    },
+    microcopy: {
+      button: "You got this, champ!",
+      tooltip: "Keep it up!",
+    },
+  },
+  // Add more personas here...
+];
